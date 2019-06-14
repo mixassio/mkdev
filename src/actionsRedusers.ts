5dd444220083af9d520870a8d699fd571e839a2d
Pasechnik Vladislav <develop.pvv@gmail.com>
2019-06-14 20:14:56 +0300

    Some ts fixes

--- src/actionsRedusers.ts
+++ src/actionsRedusers.ts
@@ -1,5 +1,5 @@
-import { createSlice } from "redux-starter-kit";
-import { combineReducers } from "redux";
+import {createSlice} from "redux-starter-kit";
+import {combineReducers} from "redux";
 
 import _ from "lodash";
 
@@ -7,98 +7,108 @@ const randomInRange = (min: number, max: number): number =>
   Math.round(Math.random() * (max - min) + min);
 
 const dicePair = createSlice({
-    slice: 'dicePair',
-    initialState: [1, 1],
-    reducers: {
-        newQuestion: (state) => [randomInRange(1, 6), randomInRange(1, 6)],
-    }
+  slice: "dicePair",
+  initialState: [1, 1],
+  reducers: {
+    newQuestion: () => [randomInRange(1, 6), randomInRange(1, 6)]
+  }
 });
 
 const value = createSlice({
-    slice: 'value',
-    initialState: '',
-    reducers: {
-        setValue: (state, action) => action.payload.value,
-    },
-    extraReducers: {
-        [dicePair.actions.newQuestion.toString()]: (state) => '',
-    }
+  slice: "value",
+  initialState: "",
+  reducers: {
+    setValue: (state, action) => action.payload.value
+  },
+  extraReducers: {
+    [dicePair.actions.newQuestion.toString()]: () => ""
+  }
 });
 
 const result = createSlice({
-    slice: 'result',
-    initialState: 0,
-    reducers: {},
-    extraReducers: {
-        [dicePair.actions.newQuestion.toString()]: (state) => randomInRange(12, 20),
-    }
+  slice: "result",
+  initialState: 0,
+  reducers: {},
+  extraReducers: {
+    [dicePair.actions.newQuestion.toString()]: () => randomInRange(12, 20)
+  }
 });
-    
+
 const score = createSlice({
-    slice: 'score',
-    initialState: 0,
-    reducers: {
-        increaseScore: (state) => state + 1,
-    }
+  slice: "score",
+  initialState: 0,
+  reducers: {
+    increaseScore: state => state + 1
+  }
 });
 
 const gameOver = createSlice({
-    slice: 'gameOver',
-    initialState: false,
-    reducers: {
-        setGameOver: (state) => true,
-    }
+  slice: "gameOver",
+  initialState: false,
+  reducers: {
+    setGameOver: () => true
+  }
 });
 
+interface ScoreBoardRecord {
+  achieved: Date;
+  score: number;
+}
+
+interface PushToScoreBoardAction {
+  type: string;
+  payload: number;
+}
+
 const scoreBoard = createSlice({
-    slice: 'scoreBoard',
-    initialState: [],
-    reducers: {
-        pushToScoreBoard: (state: any, { payload }: any): [] => {
-            const { endScore }: number = payload;
-            const arrived = new Date();
-            return [...state, { arrived, score: endScore }];
-        },
-    },
+  slice: "scoreBoard",
+  initialState: [],
+  reducers: {
+    pushToScoreBoard: (
+      state: ScoreBoardRecord[],
+      { payload }: PushToScoreBoardAction
+    ): void => {
+      const achieved = new Date();
+      state.push({ achieved, score: payload });
+    }
+  }
 });
 
 const timeLeft = createSlice({
-    slice: 'timeLeft',
-    initialState: 100,
-    reducers: {
-        increaseTimer: (state, { payload }) => {
-            const penalty = _.has(payload, "penalty") ? payload.penalty : null;
-            let newValue = state + 1;
-            if (penalty) {
-            newValue = penalty + state > 100 ? 100 : penalty + state;
-            }
-            return newValue;
-        },
-        decreaseTimer: (state, { payload }) => {
-            const penalty = _.has(payload, "penalty") ? payload.penalty : null;
-            const newValue = penalty ? state - penalty : state - 1;
-            return newValue;
-        },
+  slice: "timeLeft",
+  initialState: 100,
+  reducers: {
+    increaseTimer: (state, { payload }) => {
+      const penalty = _.has(payload, "penalty") ? payload.penalty : null;
+      let newValue = state + 1;
+      if (penalty) {
+        newValue = penalty + state > 100 ? 100 : penalty + state;
+      }
+      return newValue;
+    },
+    decreaseTimer: (state, { payload }) => {
+      const penalty = _.has(payload, "penalty") ? payload.penalty : null;
+      return penalty ? state - penalty : state - 1;
     }
   }
-);
+});
 
 export const actions = {
-    dicePairActions: dicePair.actions,
-    valueActions: value.actions,
-    resultActions: result.actions,
-    scoreActions: score.actions,
-    scoreBoardActions: scoreBoard.actions,
-    gameOverActions: gameOver.actions,
-    timeLeftActions: timeLeft.actions
+  dicePairActions: dicePair.actions,
+  valueActions: value.actions,
+  resultActions: result.actions,
+  scoreActions: score.actions,
+  scoreBoardActions: scoreBoard.actions,
+  gameOverActions: gameOver.actions,
+  timeLeftActions: timeLeft.actions
 };
 
 export default combineReducers({
-    dicePair: dicePair.reducer,
-    value: value.reducer,
-    result: result.reducer,
-    score: score.reducer,
-    scoreBoard: scoreBoard.reducer,
-    gameOver: gameOver.reducer,
-    timeLeft: timeLeft.reducer
-  });
+  dicePair: dicePair.reducer,
+  value: value.reducer,
+  result: result.reducer,
+  score: score.reducer,
+  scoreBoard: scoreBoard.reducer,
+  gameOver: gameOver.reducer,
+  timeLeft: timeLeft.reducer
+});
